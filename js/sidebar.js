3ba48d512313588c9018ced907dabcc1b1c82de2
lee87 <leejeenhwa87@gmail.com>
2021-03-03 14:54:29 +0900

    side bar update

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,11 +1,17 @@
-let langBtnMobile = document.getElementById("mobile_menuIcon");
-var sidebarBnt = document.getElementById("mobile_menuIcon");
+let menuBtn = document.getElementById("mobile_menuIcon");
+let menuBtnLabal = document.getElementsByClassName("mobile_menuIcon");
+let sidebarXBnt = document.getElementById("mobile_sidebar_x");
 var sidebar_teg = document.getElementsByClassName("side_bar")
+var mobileBackground = document.getElementsByClassName("mobile_background")
 
-langBtnMobile.addEventListener("click",function (){
-  if(sidebarBnt.checked){
+menuBtn.addEventListener("click",function (){
     sidebar_teg[0].style.left = 0;
-  }else{
-    sidebar_teg[0].style.left = -1000+'px'
-  }
+    menuBtnLabal[1].style.visibility="hidden";
+    mobileBackground[0].style.visibility="visible";
+});
+
+sidebarXBnt.addEventListener("click",function (){
+    sidebar_teg[0].style.left = -1000+'px';
+    menuBtnLabal[1].style.visibility="visible";
+    mobileBackground[0].style.visibility="hidden";
 });
