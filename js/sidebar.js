ca67c926a1dc04950e11566b6a6ca2480557d2f2
lee87 <leejeenhwa87@gmail.com>
2021-03-08 14:03:16 +0900

    모바일 사이드바 화면이동 눌렀을때 사이드바 사라지게 수정

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,6 +1,8 @@
 let menuBtn = document.getElementById("mobile_menuIcon");
 let menuBtnLabal = document.getElementsByClassName("mobile_menuIcon");
 let sidebarXBnt = document.getElementById("mobile_sidebar_x");
+let dotBnt = document.getElementById("dot_bnt");
+
 var sidebar_teg = document.getElementsByClassName("side_bar")
 var mobileBackground = document.getElementsByClassName("mobile_background")
 
@@ -14,4 +16,12 @@ sidebarXBnt.addEventListener("click",function (){
     sidebar_teg[0].style.left = -1000+'px';
     menuBtnLabal[1].style.visibility="visible";
     mobileBackground[0].style.visibility="hidden";
+});
+
+dotBnt.addEventListener("click",function (e){
+    if(e.target && e.target.nodeName == "LI") {
+        sidebar_teg[0].style.left = -1000+'px';
+        menuBtnLabal[1].style.visibility="visible";
+        mobileBackground[0].style.visibility="hidden";
+    }
 });
