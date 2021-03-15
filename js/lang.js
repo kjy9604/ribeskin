143b539038981994e655465dd22eeb4271bf9819
lee87 <leejeenhwa87@gmail.com>
2021-03-16 00:15:52 +0900

    영문페이지로 맞춤

--- js/lang.js
+++ js/lang.js
@@ -215,7 +215,7 @@ window.onload = () => {
     };
 
     //영문으로 실행하기 위해서 필요.
-    setLanguage('ko');
+    setLanguage('en');
 
     langBtn.addEventListener("click",function (){
         setLanguage(langBtn.dataset.lang);
